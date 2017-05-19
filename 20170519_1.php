65b4d06c139f52741a8e643c2eceacaff2de8d9f
Seven_Tsai <mvpscottjon@hotmail.com>
2017-05-19 17:35:38 +0800

    201705_v1

--- 20170519_1.php
+++ 20170519_1.php
@@ -8,23 +8,31 @@
 //    }
 //
 
-    if (isset($_GET[chosen])){
 
+$p = $_GET['chosen'];
+$x = $_GET['x'];
+$y = $_GET['y'];
+if ($p == 0){
+    $times = $x + $y;
+    echo $times;}
+        else if($p == 1){
+        $minus = $x - $y;
+        echo $minus;}
+            else if($p == 2){
+            $times = $x * $y;
+            echo $times;    }
+                else if($p == 4){
+                    $divided = $x % $y;
+                    echo $divided;
+                }
 
-        $x = $_GET['x'];
-        $y = $_GET['y'];
-        $p = $_GET['chosen'];
-
-
-        $col = $x + $y;
+//        $col = "1 '$p' 2";
 //        $minus = $x - $y;
-//        $times = $x * $y;
+
 //        $divided = $x / $y;
 
-        echo $col;
 
 
-    }
 ?>
 
 
@@ -35,10 +43,10 @@
     echo $x;
     ?>">
     <select name="chosen">
-        <option id="plus" value="+">+</option>
-        <option id="minus" value="-">-</option>
-        <option id="times" value="*">*</option>
-        <option id="divided" value="/"">/</option>
+        <option id="plus" value="0">+</option>
+        <option id="minus" value="1">-</option>
+        <option id="times" value="2">*</option>
+        <option id="divided" value="3"">/</option>
     </select>
 
     <input type="text" name="y" id="y" value="<?php
