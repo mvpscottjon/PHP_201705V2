0e4db1ce66d3be49b5af4b3bef0af5fc828696e0
Seven <mvpscottjon@hotmail.com>
2017-05-24 01:34:49 +0800

    20170523_HOME_V2

--- 20170523_HW4.php
+++ 20170523_HW4.php
@@ -4,17 +4,66 @@
 
 
 ?>
-<table border="1"  width="100%">
-    <tr >
-        <td>1</td>
-        <td>2</td>
-        <td>3</td>
-        <td>4</td>
-        <td>5</td>
-        <td>6</td>
+<table border="1"  width="100%" height="100%" >
 
 
-    </tr>
+    <?php
+
+    for($n=0;$n<=9;$n++){
+
+    echo ' <tr>';
+
+
+    for($i=1+10*$n;$i<=10*($n+1);$i++){
+
+
+
+        for($k=1;$k<$i;$k++){
+
+            $isCross=false;
+
+            $r =$i%$k;
+
+            if ($r !==0 ){
+                $isCross= true;
+              break;
+            } else{$isCross = false;}
+
+
+
+        }
+
+        if ($isCross){
+            echo '<td bgcolor="#ffb6c1">';
+
+            echo "$i";
+
+            echo '</td>';
+
+        }else {
+            echo '<td bgcolor="white">';
+
+            echo "$i";
+
+            echo '</td>';
+
+        }
+
+
+
+
+    }
+
+    echo ' </tr>';
+
+
+
+    }
+    ?>
+
+
+
+
 
 
 </table>
