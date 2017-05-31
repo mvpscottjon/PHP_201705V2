61a833531a2d5e4ec62d0a74ae5741c3ddb98bad
Seven <mvpscottjon@hotmail.com>
2017-06-01 02:16:18 +0800

    20170601_HOME_V1

---
+++ HW8_20170531_PDO.php
@@ -0,0 +1,64 @@
+<?php
+
+include 'sqlPdo.php';
+
+
+$PDO = @new PDO($dsn, $username, $passwd, $options);
+
+$sql = 'select * from member';
+
+$rs = $PDO->query($sql);
+
+
+//$db = @new mysqli('127.0.0.1','root'
+//    ,'root','iii');
+
+
+//
+if (isset($_GET['delid'])){
+    $delid =$_GET['delid'];
+    $sql = "delete from member where id={$delid}";
+
+   $PDO->query($sql);
+
+
+//    $db->query($sql);
+//}
+//
+//
+//$sql = 'select * from member';
+//$rs = $db->query($sql);
+
+
+
+?>
+
+
+<a href="addMember_PDO.php">New</a>
+<table width="100%" border="1">
+    <tr>
+        <td>id</td>
+        <td>account</td>
+        <td>password</td>
+        <td>realname</td>
+        <td>delete</td>
+        <td>edit</td>
+    </tr>
+<!--    --><?php
+
+        while ($row = $rs->fetchObject()){
+            echo '<tr>';
+            echo "<td>{$row->id}</td>";
+            echo "<td>{$row->account}</td>";
+            echo "<td>{$row->password}</td>";
+            echo "<td>{$row->realname}</td>";
+            echo "<td><a href='?delid={$row->id}'>Del</a></td>";
+            echo "<td><a href='editMember.php?editid={$row->id}'>Edit</a></td>";
+            echo '</tr>';
+        }
+
+
+//    ?>
+
+
+</table>
